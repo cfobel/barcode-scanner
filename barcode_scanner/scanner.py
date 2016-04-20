e02e1f43845a93e421b4bf8d78861460b9563a87
Christian Fobel <christian@fobel.net>
2016-04-20 10:40:51 -0400

    Add docstrings to BarcodeScanner class

--- barcode_scanner/scanner.py
+++ barcode_scanner/scanner.py
@@ -9,8 +9,47 @@ logger = logging.getLogger(__name__)
 
 
 class BarcodeScanner(gobject.GObject):
-    gsignal('frame-update', object)
-    gsignal('symbols-found', object, object)
+    '''
+    GObject barcode scanner class, which can scan frames from a GStreamer
+    pipeline for barcodes.
+
+    Usage
+    -----
+
+        scanner = BarcodeScanner(<`gst-launch` pipeline command>)
+
+        # Start GStreamer pipeline.
+        scanner.start()  # Emits `frame-update` signal for every video frame
+        # Start scanning each frame for barcode(s).
+        scanner.enable_scan()  # Emits `symbols-found` signal if symbols found
+
+        # Stop scanning frames for barcode(s).
+        scanner.disable_scan()
+        # Pause GStreamer pipeline, but do not release video source.
+        scanner.pause()
+        # Stop GStreamer pipeline (e.g., free webcam).
+        scanner.stop()
+
+    Signals
+    -------
+
+     - `frame-update`: `(scanner, np_img)`
+         * `scanner` (`BarcodeScanner`): Scanner object.
+         * `np_img` (`numpy.ndarray`): Video frame, with shape of
+           `(height, width, channels)`.
+     - `symbols-found`: `(scanner, np_img, symbols)`
+         * `scanner` (`BarcodeScanner`): Scanner object.
+         * `np_img` (`numpy.ndarray`): Video frame containing found symbols,
+           with shape of `(height, width, channels)`.
+         * `symbols` (`list`): List of symbol record dictionaries.  Each
+           record contains the following:
+             - `type` (`str`): Type of `zbar` code (e.g., `QRCODE`).
+             - `data` (`str`): Data from `zbar` symbol.
+             - `symbol` (`zbar.Symbol`): Symbol object.
+             - `timestamp` (`str`): UTC timestamp in ISO 8601 format.
+    '''
+    gsignal('frame-update', object)  # Args: `(scanner, np_img)`
+    gsignal('symbols-found', object, object)  # Args: `(scanner, np_img, symbols)`
 
     def __init__(self, pipeline_command=None):
         super(BarcodeScanner, self).__init__()
@@ -34,10 +73,11 @@ class BarcodeScanner(gobject.GObject):
         self.scan_id = None
         self.pipeline = None
 
-    def reset(self):
-        self.status = {'processing_frame': False,
-                       'processing_scan': False}
+    def __dealloc__(self):
+        self.stop()
 
+    ###########################################################################
+    # Callback methods
     def process_frame(self, obj, np_img):
         import PIL.Image
         import zbar
@@ -72,18 +112,44 @@ class BarcodeScanner(gobject.GObject):
         self.status['np_img'] = np_img
         self.status['processing_scan'] = False
 
-    def enable_scan(self):
-        self.scan_id = self.connect('frame-update', self.process_frame)
-
+    ###########################################################################
+    # Control methods
     def disable_scan(self):
+        '''
+        Stop scanning frames for barcode(s).
+        '''
         if self.scan_id is not None:
             self.disconnect(self.scan_id)
             self.scan_id = None
         self.reset()
 
-    def start(self, pipeline_command=None, enable_scan=False):
+    def enable_scan(self):
+        '''
+        Start scanning each frame for barcode(s).
+        '''
+        self.scan_id = self.connect('frame-update', self.process_frame)
+
+    def pause(self):
+        '''
+        Pause GStreamer pipeline, but do not release video source.
+        '''
         import gst
 
+        if self.pipeline is not None:
+            self.pipeline.set_state(gst.STATE_PAUSED)
+
+    def reset(self):
+        self.status = {'processing_frame': False,
+                       'processing_scan': False}
+
+    def start(self, pipeline_command=None, enable_scan=False):
+        '''
+        Start GStreamer pipeline and configure pipeline to trigger
+        `frame-update` for every new video frame.
+        '''
+        import gst
+
+        self.reset()
         if pipeline_command is None:
             if self.pipeline_command is None:
                 raise ValueError('No default pipeline command available.  Must'
@@ -97,7 +163,6 @@ class BarcodeScanner(gobject.GObject):
         pipeline = gst.parse_launch(unicode(pipeline_command).encode('utf-8'))
         self.pipeline = pipeline
         app = pipeline.get_by_name('app-video')
-
         self.reset()
 
         def on_new_buffer(appsink):
@@ -121,13 +186,10 @@ class BarcodeScanner(gobject.GObject):
             self.enable_scan()
         return pipeline, self.status
 
-    def pause(self):
-        import gst
-
-        if self.pipeline is not None:
-            self.pipeline.set_state(gst.STATE_PAUSED)
-
     def stop(self):
+        '''
+        Stop GStreamer pipeline (e.g., free webcam).
+        '''
         import gst
 
         self.pause()
@@ -135,7 +197,3 @@ class BarcodeScanner(gobject.GObject):
             self.pipeline.set_state(gst.STATE_NULL)
             del self.pipeline
             self.pipeline = None
-
-    def __dealloc__(self):
-        self.stop()
-
